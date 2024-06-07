87668b161405021f19da521eefb1c1b116aa35bd
Oscar Ramos <131794552+Elchamos64@users.noreply.github.com>
2024-06-07 12:36:13 -0600

    added the back to top botton

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import ImageComponent from './components/ImageComponent';
 import RenderHeader from './components/RenderHeader';
 import AboutMe from './components/AboutMe';
 import Links from './components/Links';
+import BackToTopButton from './components/BackToTopBotton';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <ImageComponent />
       <AboutMe />
       <Links />
+      <BackToTopButton />
     </div>
   );
 }
---
+++ src/components/BackToTopBotton.js
@@ -0,0 +1,42 @@
+// src/components/BackToTopButton.js
+import React, { useState, useEffect } from 'react';
+
+const BackToTopButton = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  // Show button when page is scrolled down
+  const toggleVisibility = () => {
+    if (window.pageYOffset > 300) {
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+  };
+
+  // Scroll to the top of the page
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', toggleVisibility);
+    return () => {
+      window.removeEventListener('scroll', toggleVisibility);
+    };
+  }, []);
+
+  return (
+    <div className="back-to-top">
+      {isVisible && (
+        <button onClick={scrollToTop}>
+          ↑
+        </button>
+      )}
+    </div>
+  );
+};
+
+export default BackToTopButton;
